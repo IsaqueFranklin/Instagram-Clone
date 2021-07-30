7bc329b595a2d66107b5448f0e702fb32e10bbcf
Isaque Franklin <isaquefrankli@gmail.com>
2021-07-30 18:33:47 -0300

    using reducers

--- screens/AuthScreens/Login.js
+++ screens/AuthScreens/Login.js
@@ -2,11 +2,15 @@ import {Component } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View, Dimensions } from 'react-native';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux'
+import { updateEmail, updatePassword } from '../../actions/user'
+
 
 const screenHeight = Dimensions.get('window').height
 const screenWidth = Dimensions.get('window').width
 
-export default class Login extends React.Component {
+class Login extends React.Component {
   
   render(){
     return (
@@ -21,6 +25,8 @@ export default class Login extends React.Component {
           style={{height: 50, width: screenWidth*0.9, color: 'black', paddingHorizontal: 20, margin: 0, borderRadius:10, borderColor: 'grey', borderWidth: 1}}
           placeholderTextColor={'grey'} 
           placeholder={'example@example.com'} />
+          onChangeText={input=>this.props.updateEmail(input)}
+          value={this.props.user.email}
 
           <View style={{width:screenWidth*0.9, marginTop:10,}}>
               <Text style={{left:15}}>Password</Text>
@@ -29,7 +35,10 @@ export default class Login extends React.Component {
           <TextInput 
           style={{height: 50, width: screenWidth*0.9, color: 'black', paddingHorizontal: 20, margin: 0, borderRadius:10, borderColor: 'grey', borderWidth: 1}}
           placeholderTextColor={'grey'} 
-          placeholder={'Passcode123'} />
+          placeholder={'Passcode123'} 
+          onChangeText={input=>this.props.updatePassword(input)}
+          value={this.props.user.password}
+          securityTextEntry={true} />
 
           <View style={{width: '100%', justifyContent: 'center', alignItems: 'center', marginTop: 30}}>
             <TouchableOpacity style={{width: screenWidth*0.6, height: 50, borderRadius: 30, backgroundColor: '#0095f6', justifyContent: 'center', alignItems: 'center'}}>
@@ -58,3 +67,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ updateEmail, updatePassword }, dispatch)
+}
+
+const mapStateToProps = (state) => {
+  return{
+    user: state.user,
+    post: state.post
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
