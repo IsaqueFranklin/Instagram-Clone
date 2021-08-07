7ff7082b50258a4af54e9b0cb52d580aea82f099
Isaque Franklin <isaquefrankli@gmail.com>
2021-08-06 21:16:12 -0300

    uploading post screen

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,16 +1,49 @@
-import * as React from 'react';
+import React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Dimensions} from 'react-native';
+import { FontAwesome } from '@expo/vector-icons'
 
 import { createStackNavigator } from '@react-navigation/stack';
 import TabNavigator from './TabNavigator';
 import PostCheckout from '../screens/TabScreens/upload/PostCheckout'
+import { uploadPost } from '../actions/post'
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux'
 
 const Stack = createStackNavigator();
 
-export default function MyStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="TabNavigator" component={TabNavigator} options={{headerShown: false}} />
-      <Stack.Screen name="PostCheckout" component={PostCheckout} options={{headerShown: true}} />
-    </Stack.Navigator>
-  );
+class MyStack extends React.Component {
+
+  render() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="TabNavigator" component={TabNavigator} options={{headerShown: false}} />
+        <Stack.Screen name="PostCheckout" component={PostCheckout} 
+        options={{
+          headerShown: true, 
+          headerTitle: 'See your post',
+          headerRight: () => (
+              <TouchableOpacity style={{margin:22, flexDirection: 'row'}}
+              onPress={()=> this.props.uploadPost()}>
+                <Text style={{color: 'blue', fontWeight: 'bold', fontSize:22, marginHorizontal:5, bottom:0}}>Post</Text>
+                  <FontAwesome name='check' color={'blue'} size={25} style={{top:2}} />
+              </TouchableOpacity>
+          )
+          }} />
+      </Stack.Navigator>
+    );
+  }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ uploadPost }, dispatch)
+}
+
+const mapStateToProps = (state) => {
+  return{
+      user: state.user,
+      post: state.post
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyStack)
